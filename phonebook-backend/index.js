94f30e0d9db323daad408a5325348b9297c72bc8
Ronaldo <ronaldoteran2407@gmail.com>
2025-08-14 17:35:12 -0500

    phonebook: formatting changes

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,40 +2,39 @@ const express = require("express");
 const app = express();
 
 let notes = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+	{
+		id: "1",
+		name: "Arto Hellas",
+		number: "040-123456",
+	},
+	{
+		id: "2",
+		name: "Ada Lovelace",
+		number: "39-44-5323523",
+	},
+	{
+		id: "3",
+		name: "Dan Abramov",
+		number: "12-43-234345",
+	},
+	{
+		id: "4",
+		name: "Mary Poppendieck",
+		number: "39-23-6423122",
+	},
 ];
 
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${notes.length} people</p>
-        <p>${new Date()}</p>`)
-})
+app.get("/info", (req, res) => {
+	res.send(`<p>Phonebook has info for ${notes.length} people</p>
+        <p>${new Date()}</p>`);
+});
 
-app.get('/api/persons', (req, res) => {
-    res.json(notes);
-})
-
+app.get("/api/persons", (req, res) => {
+	res.json(notes);
+});
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
-    console.log("Server running on port:", PORT);
-})
+	console.log("Server running on port:", PORT);
+});
